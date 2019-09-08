abf7e8c0b9b79fdae33cbdaa2ba6cdab9ee599c4
KritikaIyer17 <54851819+KritikaIyer17@users.noreply.github.com>
2019-09-08 20:13:57 +0530

    Update docker_script.sh

--- Week2/docker_script.sh
+++ Week2/docker_script.sh
@@ -1,29 +1,13 @@
 #Commands used-
-#Pulling NGINX image
 docker pull nginx
-
-#running the nginx image and assigning a name to the container
 docker run -it -p 80:80 --name nginx1 nginx
-
-#start the container, if needed
 docker start nginx1
-
-#docker ps shows a list of containers running
 docker ps
-
-#to log into a container
 docker exec -it nginx1 bash
-
-#change to correct path
 cd usr/share/nginx/html/
-
-#view the default index.html - this is displayed in your browser once NGINX is succesfully up and running
 cat index.html
-
-#exit the container
 exit
 
-
-
-
-
+docker tag 5a3221f0137b kritika1708/nginx:Team10
+docker login
+docker push kritika1708/nginx:Team10
